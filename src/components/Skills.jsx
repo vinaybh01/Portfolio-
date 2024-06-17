11c2b151d99e6c9b6fe786aa6b8f2d24fed1e04d
vinaybh01 <vinaybh0107@gmail.com>
2024-06-17 16:49:32 +0530

    Changed UI of skills Page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,8 @@
-// import content
-import { createElement, useState } from "react";
+import React, { useState } from "react";
 import { content } from "../Content";
-// import modal package
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Modal from "react-modal";
 
 const customStyles = {
@@ -26,48 +27,95 @@ const Skills = () => {
   const [modalIsOpen, setIsOpen] = useState(false);
   const [selectSkill, setSelectSkill] = useState(null);
 
-  function openModal() {
-    setIsOpen(true);
-  }
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
-  function closeModal() {
+  const openModal = (skill) => {
+    setSelectSkill(skill);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
     setIsOpen(false);
-  }
+  };
 
   return (
     <section className="min-h-fit bg-bg_light_primary" id="skills">
       {/* modal */}
-      {/* <Modal style={customStyles}>
-        <div className="flex items-center  gap-2">
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <div className="flex items-center gap-2">
           <img className="h-10" src={selectSkill?.logo} alt="..." />
           <h6>{selectSkill?.name}</h6>
         </div>
-      </Modal> */}
+      </Modal>
 
       {/* content */}
-      <div className="md:container px-5  py-14">
+      <div className="md:container px-5 py-14">
         <h2 className="title">{skills.title}</h2>
         <h4 className="subtitle text-[22px] sm:text-[28px]">
           {skills.subtitle}
         </h4>
         <br />
         <div className="flex flex-wrap gap-10">
-          {skills.skills_content.map((skill, i) => (
-            <div
-              key={i}
-              className="sm:cursor-pointer relative group w-[110px] sm:w-[145px] flex items-center gap-5 p-5 "
-            >
-              {" "}
-              <div className="">
-                <div className="flex items-center justify-center">
-                  <img src={skill.logo} alt="..." className="w-8 sm:w-10 m-2" />
+          <div style={{ width: "100%" }}>
+            <Slider {...settings}>
+              {skills.skills_content.map((skill, i) => (
+                <div key={i} className="p-6">
+                  <div
+                    onClick={() => openModal(skill)}
+                    className="sm:cursor-pointer relative group w-[110px] sm:w-[145px] flex items-center gap-5 p-5 flex-col"
+                  >
+                    <div className="flex items-center justify-center">
+                      <img
+                        src={skill.logo}
+                        alt="..."
+                        className="w-8 sm:w-10 m-2"
+                      />
+                    </div>
+                    <div>
+                      <h6 className="text-[12px] sm:text-[16px]">
+                        {skill.name}
+                      </h6>
+                    </div>
+                  </div>
                 </div>
-                <div>
-                  <h6 className="text-[12px] sm:text-[16px]">{skill.name}</h6>
-                </div>
-              </div>
-            </div>
-          ))}
+              ))}
+            </Slider>
+          </div>
         </div>
       </div>
     </section>
